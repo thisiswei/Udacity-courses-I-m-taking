db558ac794289d10aa67bdfd1a91764bec924c59
thisiswei <99plus2@gmail.com>
2012-11-04 23:44:42 -0800

    ..

---
+++ CS215-Algorithm/unit3/unit3-19 Centrality.py
@@ -0,0 +1,17 @@
+# continue the concert in unit 3 ,and caculate the average of the distance from note V to everyother nodes in Graph G
+
+
+
+def centrality(G,v):
+    distance_from_start={}
+    linked = [v]
+    distance_from_start[v]=0
+    while len(linked)>0:
+        current = linked[0]
+        del linked[0]
+        for neighbor in G[current].keys():
+            if neighbor not in distance_from_start:
+                distance_from_start[neighbor] = distance_from_start[current]+1
+                linked.append(neighbor)
+    return sum(distance_from_start.values()+0.0)/len(distance_from_start)
+
