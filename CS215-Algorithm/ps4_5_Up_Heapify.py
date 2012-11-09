8289b0b8a508fd982ab87e14913ccffa98702b93
thisiswei <99plus2@gmail.com>
2012-11-09 01:56:15 -0800

    nov/09/12 1:56 about to sleep...

---
+++ CS215-Algorithm/ps4_5_Up_Heapify.py
@@ -0,0 +1,25 @@
+#
+# write up_heapify, an algorithm that checks if
+# node i and its parent satisfy the heap
+# property, swapping and recursing if they don't
+#
+# L should be a heap when up_heapify is done
+#
+
+def up_heapify(L, i):
+    if i == 0: return
+    if L[parent(i)] <= L[i]:return
+    (L[parent(i)],L[i]) = (L[i],L[parent(i)])
+    up_heapify(L,parent(i))
+    
+
+def parent(i): 
+    return (i-1)/2
+def left_child(i): 
+    return 2*i+1
+def right_child(i): 
+    return 2*i+2
+def is_leaf(L,i): 
+    return (left_child(i) >= len(L)) and (right_child(i) >= len(L))
+def one_child(L,i): 
+    return (left_child(i) < len(L)) and (right_child(i) >= len(L))
