27bb9d3786407968033a49ff5267786a0d0a1de3
thisiswei <99plus2@gmail.com>
2013-01-02 00:58:33 -0800

    sleep now

--- CS253/hackerschoolrocks/main.py
+++ CS253/hackerschoolrocks/main.py
@@ -1,30 +1,52 @@
+
 import webapp2 
 import jinja2
 import os
 from google.appengine.ext import db
+import urllib2
+from xml.dom import minidom
+
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)  
 
+URL = 'http://api.hostip.info/?ip='
+GMAPS_URL = "http://maps.googleapis.com/maps/api/staticmap?size=380x263&sensor=false&"
 
-def render_str(template, **params):
-    t = jinja_env.get_template(template)
-    return t.render(params)
 
-class Post(db.Model):
-    subject = db.StringProperty(required = True)
-    content = db.TextProperty(required = True)
+def get_coords(ip):
+    url = URL + ip
+    content = None
+    try:
+        content = urllib2.urlopen(url).read()
+    except URLError:
+        return
+    if content:
+        d = minidom.parseString(content)
+        coords = d.getElementsByTagName("gml:coordinates")
+        if coords and coords[0].childNodes[0].nodeValue:
+            lon, lat = coords[0].childNodes[0].nodeValue.split(',')
+            return db.GeoPt(lat, lon)
+
+def gmaps_img(points):
+    markers = '&'.join('markers=%s,%s' %(p.lat, p.lon)
+                       for p in points)
+    return  GMAPS_URL + markers
+
+
+
+class Art(db.Model):
+    title = db.StringProperty(required = True)
+    art = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
-
-    def render(self):
-        self._render_text = self.content.replace('\n', '<br>')
-        return render_str('post.html', p=self)
+    coords = db.GeoPtProperty()
 
 class BaseHandler(webapp2.RequestHandler): 
     def render_str(self, template, **params):
-        return render_str(template, **params)
-
+        t = jinja_env.get_template(template)
+        return t.render(params)   
+                 
     def render(self, template, **kw):
         self.response.write(self.render_str(template, **kw))    
     
@@ -32,45 +54,35 @@ class BaseHandler(webapp2.RequestHandler):
         self.response.write(*a, **kw)
 
 
-class BlogFront(BaseHandler): 
-    def get(self):
-        posts = db.GqlQuery('select * from Post order by created DESC') 
-        self.render('blog_front.html', posts=posts)
+class MainHandler(BaseHandler):
+    def render_ascii(self, title='', art='', error=''):
+        arts = db.GqlQuery('select * from Art order by created DESC LIMIT 10')
+        arts = list(arts)
+        points = filter(None, (a.coords for a in arts))
+        img_url = None
+        if points:
+            img_url = gmaps_img(points)
+        self.render('asi.html', title = title, art = art, error = error, arts =
+                   arts, img_url = img_url)
 
-
-class NewPost(BaseHandler):
     def get(self):
-        self.render('new_post.html')
+        self.render_ascii()
 
     def post(self):
-        subject = self.request.get('subject')
-        content = self.request.get('content')
-
-        if subject and content:
-            p = Post(subject = subject, content = content) 
-            p.put()
-            self.redirect('/blogs/'+str(p.key().id()))
+        title = self.request.get('title')
+        art = self.request.get('art')
+        if title and art:
+            a = Art(title = title, art = art)
+            coords = get_coords(self.request.remote_addr)
+            if coords:
+                a.coords = coords
+            a.put()
+            self.redirect('/')
         else:
             error = 'wtf, both needed'
-            self.render('new_post.html', subject=subject, content=content, error=error) 
-
-class PostPage(BaseHandler):
-    def get(self, post_id):
-        key = db.Key.from_path('Post', int(post_id))
-        post = db.get(key)
-        if not post:
-            self.error(404)
-            return
-        self.render('post_id.html', post=post)
-
-class MainHandler(BaseHandler):
-    def get(self):
-        self.response.write('wtf')
+            self.render_ascii(title, art, error)
 
 app = webapp2.WSGIApplication([
-    ('/blogs', BlogFront),
-    ('/blogs/newpost', NewPost),
-    ('/blogs/([0-9]+)',PostPage)
+    ('/', MainHandler)
 ], debug=True)   
-
 
